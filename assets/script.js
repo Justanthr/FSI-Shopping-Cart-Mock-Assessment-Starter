0f875be4f30425c8f03b460b018a9cab7d6b599f
Jennika Wilson <j.r.a.112100@gmail.com>
2022-01-29 02:42:11 -0500

    in process. Adding total

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
-let totalQuantity = 1
+body = document.querySelector('body')
+ let totalQuantity = 1
+let totalCost = 15.55
+
+console.log(totalCost)
 
 //Change quantity dispayed
 function changeQuantity(displayQuantity) {
     let totalQuantity = document.querySelector('.total-quantity')
-    totalQuantity.textContent = displayQuantity 
+    totalQuantity.textContent = displayQuantity   
 }
 
 //Quantity up
@@ -28,3 +32,40 @@ removeBtn.addEventListener('click', function(){
     totalQuantity = 1
     changeQuantity(`Quantity: ${totalQuantity}`)
 })
+
+//Checkout hide button
+checkOut = document.querySelector('.btn-checkout')
+checkOut.addEventListener('click', function(){
+    total = document.querySelector('#cost');
+    let btnText = checkOut.textContent
+    if(btnText === 'Checkout'){
+        total.style.display = 'block';
+        checkOut.innerHTML = 'Back'
+    }
+    else{
+        total.style.display = 'none'
+        checkOut.innerHTML = 'Checkout'
+    }
+})
+
+//Adding cost total 
+let cost = document.createElement('div')
+cost.setAttribute('id', 'cost')
+cost.setAttribute('style', 'display: none')
+
+let costTotal = document.createElement('span')
+costTotal.setAttribute('id', 'total-cost')
+costTotal.textContent = 'Total: 15.55'
+
+body.append(cost)
+cost.append(costTotal)
+
+//Change price displayed
+// function changeTotal(displayTotal) {
+//     let totalCost = document.querySelector('#total-cost')
+//     totalCost.textContent = displayTotal
+// }
+
+// hangeTotal(`Total: ${totalCost}`)
+
+//Adding quantity total
