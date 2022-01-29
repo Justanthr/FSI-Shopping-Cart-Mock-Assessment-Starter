f361120b3ae6483b21627a2def4f9cb0887fff54
Jennika Wilson <j.r.a.112100@gmail.com>
2022-01-28 20:30:40 -0500

    done with the basics

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
 let totalQuantity = 1
 
+//Change quantity dispayed
+function changeQuantity(displayQuantity) {
+    let totalQuantity = document.querySelector('.total-quantity')
+    totalQuantity.textContent = displayQuantity 
+}
 
 //Quantity up
-quantityUp = document.getElementById('quantity-up')
-quantityUp.addEventListener('click', function(){
+addQuantity = document.getElementById('quantity-up')
+addQuantity.addEventListener('click', function(){
     totalQuantity = totalQuantity + 1
+    changeQuantity(`Quantity: ${totalQuantity}`)
     console.log(totalQuantity)
 })
 
 //Quantity down
 quantityDown = document.getElementById('quantity-down')
 quantityDown.addEventListener('click', function(){
-    totalQuantity = totalQuantity - 1
-    if(totalQuantity > 0)
+    if(totalQuantity > 1){
+        totalQuantity = totalQuantity - 1
+    }
+    changeQuantity(`Quantity: ${totalQuantity}`)
     console.log(totalQuantity)
 })
